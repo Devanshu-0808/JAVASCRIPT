7ecb468ee36b7f256e97a747709001abed360475
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-01 09:52:43 +0000

    convertionofdatatypes

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,13 @@
+let score = null
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let flag=3333
+
+console.log(String(flag))
