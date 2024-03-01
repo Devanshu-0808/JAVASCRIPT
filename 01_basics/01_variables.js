7ef15dc220b922917abe51598f6fb6425dfc1305
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-01 07:44:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =144553
+let accountEmail ="devanshu@google.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountState;
+//accountId=2
+
+console.log(accountId);
+
+console.table([accountEmail , accountPassword , accountCity , accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+console.log(3+3)
+console.log("devanshu")
+
+let name ="devanshu"
+let age =21
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint 
+//string =>" "
+//boolean => true /false
+//null-> stand alone value or it is representation of empty value
+//undefined ->variable declared but not given value
+// symbol =>used for unque
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
