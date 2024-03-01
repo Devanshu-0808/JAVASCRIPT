2dd440d6cb0211bbd141c72cbebbd88246537c8a
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-01 11:46:23 +0000

    stackandheap

---
+++ 01_basics/07_stackandheap.js
@@ -0,0 +1,19 @@
+let youtubename = "devansh"
+
+let anotheryoutubename=youtubename
+anotheryoutubename="chatterjee"
+console.log(youtubename)
+console.log(anotheryoutubename)
+
+
+let user ={
+    email : "devanshu@gmail.com" ,
+    upi : "devanshu@ybl"
+}
+let userTwo=user;
+
+userTwo.email="user@gmail.com"
+
+
+console.log(user.email)
+console.log(userTwo.email)
