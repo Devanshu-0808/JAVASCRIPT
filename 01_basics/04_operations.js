8d4d626ad8359a9ecb02e1fd0f12d8f0693eab37
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-01 10:17:45 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,25 @@
+let value=3;
+let negv=-value;
+console.log(negv)
+
+let str1="devanshu"
+let str2="chatterjee"
+
+console.log(str1+str2)
+
+console.log("1" +2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let num1 , num2 , num3
+num1=num2=num3=4
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+let game=1
+console.log(++game)
+console.log(game++)
+
+
