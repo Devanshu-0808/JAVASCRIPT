34f2c523c93e6c7b2d7daedc21d912cca8fe8ad3
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-01 11:12:00 +0000

    summary

---
+++ 01_basics/06_datatypessummary.js
@@ -0,0 +1,14 @@
+//primitive ->7 types
+//String , Number , Boolean , null , undefined , Symbol , BigInt
+
+//javascript is dynamically type language means data type writing is not req
+
+//Non primitive or Reference Type 
+// Array , Objects , Functions
+
+const id= Symbol('123')
+const aid= Symbol('123')
+console.log(aid==id)
+
+const bigNumber = 343453556543n
+
