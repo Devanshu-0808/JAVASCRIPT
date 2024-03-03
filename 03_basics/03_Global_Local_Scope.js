6c18a78dff050d93bbbc3a7b6e8e7471891b455e
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-03 18:30:49 +0000

    scopes

---
+++ 03_basics/03_Global_Local_Scope.js
@@ -0,0 +1,17 @@
+
+
+//{} scope
+
+//global scope
+
+
+if(true){
+    //local scope
+    let a=10
+    const b=20
+    var c=30
+}
+
+// console.log(a);   //a work normal variable like c++
+// console.log(b);     //a work normal variable like c++
+console.log(c); // var variable comes out it is behaves as globally
