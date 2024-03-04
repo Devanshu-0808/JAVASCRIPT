e90791eb7b6beb7ea95f453a76eb34d6fa74b1a5
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-04 16:47:18 +0000

    while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,24 @@
+//while loop
+
+let i=0;
+
+while(i<10)
+{
+    console.log(i);
+    i++;
+}
+
+let arr =["flash" , "batman" , "superman"]
+let z=0
+while(z<arr.length)
+{
+    console.log(arr[z])
+    z++
+}
+
+/// do while loop
+
+let h=0
+do{
+  console.log("hello")
+}while(h!=0);
